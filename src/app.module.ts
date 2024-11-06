91b2d38ed8ecb7502d639d767c1856c5fff949db
Franco_Serjen <francoserjen100@gmail.com>
2024-11-06 10:16:50 -0300

    Instalación de mongoose con paquete para nest js y conexión a mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017')],
   controllers: [AppController],
   providers: [AppService],
+  exports: []
 })
-export class AppModule {}
+export class AppModule { }
