d64f8917ca75a962b3c45b1eda53814964774702
Franco_Serjen <francoserjen100@gmail.com>
2024-11-24 15:18:16 -0300

    Creacion de schema de indice listo

--- src/indices/models/schemas/indice.schema.ts
+++ src/indices/models/schemas/indice.schema.ts
@@ -1,1 +1,15 @@
-export class Indice {}
+import { HydratedDocument } from "mongoose";
+import { IIndice } from "../interface/indice.interface";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+export type IndiceDocument = HydratedDocument<IIndice>;
+@Schema()
+export class Indice {
+    @Prop({ required: true, unique: true })
+    id: number;
+    @Prop({ required: true })
+    code: string;
+    @Prop({ required: true })
+    name: string;
+}
+export const EmpresaSchema = SchemaFactory.createForClass(Indice);
